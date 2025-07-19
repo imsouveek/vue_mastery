1450f355ff454cd5ffc79350cbc50ba19dd997d3
imsouveek <imsouveek@gmail.com>
2025-07-19 08:23:48 +0000

    Updated formatting

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <div>
         <h1>Projects Page</h1>
-        <RouterLink to='/'>Go to Home</RouterLink>
+        <RouterLink to="/">Go to Home</RouterLink>
     </div>
 </template>
